e40c041daaa38c00f26359b8e98e82943fed3bd7
takbirul islam <takbirulislam420@gmail.com>
2025-12-05 20:48:21 +0600

    change value slider with container color

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:practice_riverpod/slider_provider.dart';
 
 // Switch must be StateProvider, not Provider.
 final switchButton = StateProvider<bool>((ref) {
@@ -11,81 +12,17 @@ final counter = StateProvider<int>((ref) {
   return 0;
 });
 
-class HomeScreen extends ConsumerStatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends ConsumerState<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-        return Scaffold(
-      appBar: AppBar(title: const Text("data")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Counter text
-          Consumer(
-            builder: (context, ref, child) {
-              print("build1 ful");
-              final count = ref.watch(counter);
-              return Center(
-                child: Text("consumer: $count", style: TextStyle(fontSize: 20)),
-              );
-            },
-          ),
-
-          // Switch
-          Consumer(
-            builder: (context, ref, child) {
-              print("build2");
-              final switchState = ref.watch(switchButton);
-              return Center(
-                child: Switch(
-                  value: switchState,
-                  onChanged: (value) {
-                    ref.read(switchButton.notifier).state = value;
-                  },
-                ),
-              );
-            },
-          ),
-
-          // + button
-          IconButton(
-            onPressed: () {
-              ref.read(counter.notifier).state++;
-            },
-            icon: const Icon(Icons.add),
-          ),
-
-          // - button
-          IconButton(
-            onPressed: () {
-              ref.read(counter.notifier).state--;
-            },
-            icon: const Icon(Icons.remove),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-
-
-// class HomeScreen extends ConsumerWidget {
+// class HomeScreen extends ConsumerStatefulWidget {
 //   const HomeScreen({super.key});
 
 //   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     print("build");
+//   ConsumerState<HomeScreen> createState() => _HomeScreenState();
+// }
 
-//     return Scaffold(
+// class _HomeScreenState extends ConsumerState<HomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//         return Scaffold(
 //       appBar: AppBar(title: const Text("data")),
 //       body: Column(
 //         mainAxisAlignment: MainAxisAlignment.center,
@@ -94,7 +31,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 //           // Counter text
 //           Consumer(
 //             builder: (context, ref, child) {
-//               print("build1");
+//               print("build1 ful");
 //               final count = ref.watch(counter);
 //               return Center(
 //                 child: Text("consumer: $count", style: TextStyle(fontSize: 20)),
@@ -138,3 +75,90 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 //     );
 //   }
 // }
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    print("build");
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("data")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Counter text
+          Consumer(
+            builder: (context, ref, child) {
+              print("build1");
+              final count = ref.watch(counter);
+              return Center(
+                child: Text("consumer: $count", style: TextStyle(fontSize: 20)),
+              );
+            },
+          ),
+
+          // Switch
+          Consumer(
+            builder: (context, ref, child) {
+              print("build2");
+              final switchState = ref.watch(switchButton);
+              return Center(
+                child: Switch(
+                  value: switchState,
+                  onChanged: (value) {
+                    ref.read(switchButton.notifier).state = value;
+                  },
+                ),
+              );
+            },
+          ),
+
+          // + button
+          IconButton(
+            onPressed: () {
+              ref.read(counter.notifier).state++;
+            },
+            icon: const Icon(Icons.add),
+          ),
+
+          // - button
+          IconButton(
+            onPressed: () {
+              ref.read(counter.notifier).state--;
+            },
+            icon: const Icon(Icons.remove),
+          ),
+
+          Consumer(
+            
+            builder: (context, ref, child) {
+              print("container consumer");
+              final sliderValue = ref.watch(sliderProvider);
+              return Container(
+                height: 200,
+                width: 200,
+                // ignore: deprecated_member_use
+                color: Colors.amber.withOpacity(sliderValue),
+              );
+            },
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              final sliderValue = ref.watch(sliderProvider);
+              print("slider consumer");
+              return Slider(
+                value: sliderValue,
+                onChanged: (value) {
+                  ref.read(sliderProvider.notifier).state = value;
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
