d6493821d2ab7e052feed3bdd80f6a7c2ba86a12
takbirul islam <takbirulislam420@gmail.com>
2025-12-05 19:21:54 +0600

    just initilaize riverPod with stateless and statefull widget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,24 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
-final hello1 =Provider<String>((ref){
-  return "hello provider pod";
+final hello1 = Provider<String>((ref) {
+  return "hello provider";
 });
 
-final hello2 =Provider<int>((ref){
-  return 25;
+final counter = StateProvider<int>((ref) {
+  return 0;
 });
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final helloRiver=ref.watch(hello1);
-    final intRiver=ref.watch(hello2);
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final helloRiver = ref.watch(hello1);
     return Scaffold(
-      appBar: AppBar(title: Text("data"),),
-      body: Center(child: Text("consumer: $intRiver $helloRiver"),),
-      );
+      appBar: AppBar(title: Text("data")),
+      body: Center(child: Text("consumer: $helloRiver")),
+    );
   }
 }
+
+
+// Work with RiverPod
+ 
+// class HomeScreen extends ConsumerWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final helloRiver=ref.watch(hello1);
+//     final intRiver=ref.watch(hello2);
+//     return Scaffold(
+//       appBar: AppBar(title: Text("data"),),
+//       body: Center(child: Text("consumer: $intRiver $helloRiver"),),
+//       );
+//   }
+// }
